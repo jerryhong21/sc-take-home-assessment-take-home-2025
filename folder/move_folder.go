b71587829e0b37a1f718add6c7b2ca5c00d38e54
Jerry Hong <jh1012037421@gmail.com>
2024-10-15 23:44:31 +1100

    MoveFolder() implementation: refactored MoveFolder to modularise error checking and other logic in move_folder.go

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+
 /* Error checkings
 1. Moving a node to a child of itself DONE
 2. Moving a folder to itself DONE
@@ -18,51 +19,21 @@ import (
 // a specific pair of folders with the names "name1" and "name2".
 func (d *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 
+	// check if src and dst are the same
 	if name == dst {
 		return nil, fmt.Errorf("moveFolder: cannot move folder to itself")
 	}
-	// validate folder names
 
-	// check for existence, obtain their folder pointers
-	srcNameFolders := d.nameIndex[name]
-	destNameFolders := d.nameIndex[dst]
-
-	// temp map to store all the seen orgIds with sourceName
-	srcNameOrgIds := make(map[uuid.UUID]bool)
-	for _, folder := range srcNameFolders {
-		srcNameOrgIds[folder.OrgId] = true
-	}
-	var matchingOrgId uuid.UUID
-	orgIdMatched := false
-	// look for a folder with dest name, and orgId
-	for _, folder := range destNameFolders {
-		// check if there exists a srcName folder with orgId of the destNameFolder
-		_, found := srcNameOrgIds[folder.OrgId]
-		// if found, then its a valid match with folder.OrgId, destName and srcName
-		if found {
-			matchingOrgId = folder.OrgId
-			orgIdMatched = true
-		}
-	}
-	if !orgIdMatched {
-		return nil, fmt.Errorf("moveFolder: No matching orgId found between '%s' and '%s'", name, dst)
+	// get matching orgId
+	matchingOrgId, err := d.getMatchingOrgId(name, dst)
+	if err != nil {
+		return nil, fmt.Errorf("moveFolder: %v", err)
 	}
 
-	// obtain folder pointers for name and dst
-	var srcFolder *Folder
-	var dstFolder *Folder
-	for _, folder := range d.orgIdIndex[matchingOrgId] {
-		f := folder
-		if f.Name == name {
-			srcFolder = f
-		} else if f.Name == dst {
-			dstFolder = f
-		}
-	}
-	if srcFolder == nil {
-		return nil, fmt.Errorf("moveFolder: Source folder '%s' not found", name)
-	} else if dstFolder == nil {
-		return nil, fmt.Errorf("moveFolder: Destination folder '%s' not found", dst)
+	// get srcFolder and dstFolder with matchingOrgId
+	srcFolder, dstFolder, err := d.getFolders(matchingOrgId, name, dst)
+	if err != nil {
+		return nil, fmt.Errorf("moveFolder: %v", err)
 	}
 
 	// check for cycles, dst cannot be a descendent of src
@@ -87,14 +58,56 @@ func (d *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	srcFolder.Parent = dstFolder
 
 	// recursive reassign children's path
-	// oldPath := srcFolder.Paths
 	newPath := fmt.Sprintf("%s.%s", dstFolder.Paths, name)
-	fmt.Printf("New path for folder %s is %s\n", srcFolder.Paths, newPath)
 
 	// update all the paths and update pathIndex along the way
 	d.updatePaths(srcFolder, newPath)
 
 	return d.getAllFolders(), nil
+}
+
+// Function to get matchingOrgId given srcName and dstName
+func (d *driver) getMatchingOrgId(name string, dst string) (uuid.UUID, error) {
+	srcNameFolders := d.nameIndex[name]
+	destNameFolders := d.nameIndex[dst]
+
+	srcNameOrgIds := make(map[uuid.UUID]bool)
+	for _, folder := range srcNameFolders {
+		srcNameOrgIds[folder.OrgId] = true
+	}
+	var matchingOrgId uuid.UUID
+	orgIdMatched := false
+	for _, folder := range destNameFolders {
+		_, found := srcNameOrgIds[folder.OrgId]
+		if found {
+			matchingOrgId = folder.OrgId
+			orgIdMatched = true
+		}
+	}
+	if !orgIdMatched {
+		return uuid.Nil, fmt.Errorf("getMatchingOrgId: No matching orgId found between '%s' and '%s'", name, dst)
+	}
+	return matchingOrgId, nil
+}
+
+// Get srcFolder and dstFolder given orgId and folderNames
+func (d *driver) getFolders(orgId uuid.UUID, name string, dst string) (*Folder, *Folder, error) {
+	var srcFolder *Folder
+	var dstFolder *Folder
+	for _, folder := range d.orgIdIndex[orgId] {
+		if folder.Name == name {
+			srcFolder = folder
+		} else if folder.Name == dst {
+			dstFolder = folder
+		}
+	}
+	if srcFolder == nil {
+		return nil, nil, fmt.Errorf("getFolders: Source folder '%s' not found", name)
+	}
+	if dstFolder == nil {
+		return nil, nil, fmt.Errorf("getFolders: Destination folder '%s' not found", dst)
+	}
+	return srcFolder, dstFolder, nil
 }
 
 // recursively updates all path names
